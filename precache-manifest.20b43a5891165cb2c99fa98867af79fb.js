0435dd98d807b66bf2b9087f1f8775efa9055dbe
Dean Perkins <deanperkins@jackentertainment.com>
2019-11-13 15:56:05 -0500

    Updates

---
+++ precache-manifest.20b43a5891165cb2c99fa98867af79fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d00d5624a3da764252e1cabdc554395",
+    "url": "/dernperkins.github.io/index.html"
+  },
+  {
+    "revision": "19653c63492d70d7a54a",
+    "url": "/dernperkins.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e831c2308883e280585a",
+    "url": "/dernperkins.github.io/static/js/2.92d0f2dd.chunk.js"
+  },
+  {
+    "revision": "19653c63492d70d7a54a",
+    "url": "/dernperkins.github.io/static/js/main.b85600ae.chunk.js"
+  },
+  {
+    "revision": "7048b6cfb67e406244fb",
+    "url": "/dernperkins.github.io/static/js/runtime-main.50de3d14.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dernperkins.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9fb0a25352ad42252b6add171ce2136.js"
+  "/dernperkins.github.io/precache-manifest.20b43a5891165cb2c99fa98867af79fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dernperkins.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
