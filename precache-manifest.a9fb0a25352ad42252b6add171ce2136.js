571ef5c9f88334c15672515392a97ee7f4d08b6c
Dean Perkins <deanperkins@jackentertainment.com>
2019-11-13 15:34:52 -0500

    Updates

---
+++ precache-manifest.a9fb0a25352ad42252b6add171ce2136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b717832ae3c50c5db3f9faf1c692bbfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e8edaa495771220a931",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e831c2308883e280585a",
+    "url": "/static/js/2.92d0f2dd.chunk.js"
+  },
+  {
+    "revision": "4e8edaa495771220a931",
+    "url": "/static/js/main.32db1a1b.chunk.js"
+  },
+  {
+    "revision": "d4512289248d5a7a48cf",
+    "url": "/static/js/runtime-main.8a6ec668.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
